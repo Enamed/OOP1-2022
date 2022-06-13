89d851ff21dd3208fe01857422ae805ae36743cb
Enamed <85908108+Enamed@users.noreply.github.com>
2022-06-13 20:52:03 +0300

    oop2

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,5 +1,29 @@
 //package ru.netology
 
 object WallService {
-    private val posts = emptyArray<Post>()
+    private var posts = emptyArray<Post>()
+    private var lastId = 1
+
+    fun add(post: Post): Post {
+        post.id = lastId++
+        posts +=post
+        return  posts.last()
+    }
+
+    fun update(post: Post): Boolean{
+        for (searchPost in posts) {
+            if (post.id == searchPost.id){
+                val updatePost = Post(searchPost.id, post.text, post.isFavorite)
+                posts[posts.indexOf(searchPost)] = updatePost
+                return true
+            }
+        }
+        return false
+    }
+    fun printPosts() {
+        for ((index, value) in posts.withIndex()) {
+            println("Значение индекса $index содержимое $value")
+        }
+    }
+
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,4 +2,21 @@
 
 fun main(){
 
+
+    val firstPost = Post(text = "Hello", isFavorite = true)
+    val secondPost = Post(text = "Kotlin", isFavorite = false)
+    val thirdPost = Post(text = "How", isFavorite = true)
+    val fourthPost = Post(text = "You are", isFavorite = true)
+
+    val updatedPost = Post(id = 3, text="edited text", isFavorite = false)
+
+    WallService.add(firstPost)
+    WallService.add(secondPost)
+    WallService.add(thirdPost)
+    WallService.add(fourthPost)
+
+    WallService.update(updatedPost)
+    println(WallService.update(secondPost))
+    //WallService.printPosts()
+
 }
